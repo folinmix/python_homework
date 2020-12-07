165f520d64ec26aedeedd2d7af7d87fce0278e1c
Nick <nickfol@mail.ru>
2020-12-07 23:32:05 +0300

    Homework of lesson_1

---
+++ lesson_1_exercise_1.py
@@ -0,0 +1,27 @@
+# Exercise 1 - Work with variables
+print("---------------------------Start of program---------------------------")
+
+# Define variables
+variable_1 = "I'm a first variable!"
+variable_2 = 5
+variable_3 = 10.55
+
+# Display variables
+print("---------Print variables---------")
+print(variable_1)
+print(variable_2)
+print(variable_3)
+
+# Input variables by user
+print("---------Get variables from user---------")
+variable_1 = input("Введите переменную типа str\n")
+variable_2 = input("Введите переменную типа int\n")
+variable_3 = input("Введите переменную типа float\n")
+
+# Display variables
+print("---------Print user's variables---------")
+print(variable_1)
+print(variable_2)
+print(variable_3)
+
+print("---------------------------End of program---------------------------")
