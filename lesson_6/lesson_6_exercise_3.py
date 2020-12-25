56bc572db0b0a81609765a4073186a8ea456c302
Nick <nickfol@mail.ru>
2020-12-25 16:17:20 +0300

    There is my homework of lesson_6

---
+++ lesson_6/lesson_6_exercise_3.py
@@ -0,0 +1,38 @@
+""" Exercise 3
+        Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+        name, surname, position (должность), income (доход).
+        Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+        оклад и премия, например, {"wage": wage, "bonus": bonus}.
+        Создать класс Position (должность) на базе класса Worker.
+        В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+        и дохода с учетом премии (get_total_income).
+        Проверить работу примера на реальных данных (создать экземпляры класса Position,
+        передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+"""
+
+
+class Worker:
+
+    def __init__(self, name: str, surname: str, position: str, wage: int, bonus: int):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name: str, surname: str, position: str, wage: int, bonus: int):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f"Полное имя сотрудника: {self.name} {self.surname}"
+
+    def get_total_income(self):
+        full_income = self._income.get("wage") + self._income.get("bonus")
+        return f"Полный доход сотрудника: {full_income}"
+
+
+Person = Position("Иоан", "Рюрикович", "царь", 100000, 50000)
+print(Person.get_full_name())
+print(Person.get_total_income())
